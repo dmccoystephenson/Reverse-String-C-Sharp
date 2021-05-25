b30f3c538aa6a61ea52e096a315e0848ca4d972a
dmccoystephenson <dmccoystephenson@gmail.com>
2021-05-25 11:47:40 -0600

    initial commit

---
+++ reverse-string.cs
@@ -0,0 +1,23 @@
+﻿namespace reverse_string_c_sharp {
+	class ReverseString {
+		static void Main(string[] args) {
+			// tell user to input a string
+			System.Console.WriteLine("Enter the string you want to reverse.");
+			
+			// get input
+			string input = System.Console.ReadLine();
+			
+			// split into character array
+			char[] array = input.ToCharArray();
+			
+			// reverse characters in array
+			System.Array.Reverse(array);
+			
+			// create new string from array
+			string reversed = new string(array);
+			
+			// output reversed string
+			System.Console.WriteLine("\nReversed String: \n" + reversed);
+		}
+	}
+}
